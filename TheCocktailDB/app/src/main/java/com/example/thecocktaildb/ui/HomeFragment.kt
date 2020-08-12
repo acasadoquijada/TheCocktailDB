21631e7142bef8a43a9f5c5c60162618d27ca984
acasadoquijada <acasadoquijada@gmail.com>
2020-08-12 13:15:32 +0200

    feature: show drink information HomeFragment.kt
    Some placeholder views have been added to the home_fragment.xml layout.
    With DataBinding, the information obtain from a random drink is shown
    in the layout.
    
    This commit add the changes needed to achieve the work described above.

--- TheCocktailDB/app/src/main/java/com/example/thecocktaildb/ui/HomeFragment.kt
+++ TheCocktailDB/app/src/main/java/com/example/thecocktaildb/ui/HomeFragment.kt
@@ -5,11 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.thecocktaildb.R
+import com.example.thecocktaildb.databinding.HomeFragmentBinding
 import com.example.thecocktaildb.viewmodel.HomeViewModel
+import com.squareup.picasso.Picasso
 
 class HomeFragment : Fragment() {
 
@@ -18,12 +21,15 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var viewModel: HomeViewModel
+    private lateinit var mBinding: HomeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.home_fragment,container,false)
+        return mBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -32,7 +38,15 @@ class HomeFragment : Fragment() {
 
         viewModel.getRandomCocktail().observe(viewLifecycleOwner, Observer { drinkList ->
             for (drink in drinkList.drinkList) {
-                Log.d("TESTING__", "DRINK: $drink")
+                Log.d("TESTING",drink.toString())
+
+                var value = drink.getIngredients();
+
+                mBinding.cocktailTitle.text = drink.name
+                Picasso.get().load(drink.image).into(mBinding.cocktailImage)
+                mBinding.category.text = drink.category
+                mBinding.glassType.text = drink.glass
+                mBinding.instructions.text = drink.instruction
             }
         })
     }
